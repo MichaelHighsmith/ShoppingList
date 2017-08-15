b1d677d79f6aeea70dcceb340f76bb95ae952c0a
Michael Highsmith <mhighsm1@slu.edu>
2017-08-14 21:58:20 -0500

    Initial commit

---
+++ app/src/main/java/com/udacity/firebase/shoppinglistplusplus/model/ShoppingList.java
@@ -0,0 +1,45 @@
+package com.udacity.firebase.shoppinglistplusplus.model;
+
+import com.fasterxml.jackson.annotation.JsonIgnore;
+import com.firebase.client.ServerValue;
+import com.udacity.firebase.shoppinglistplusplus.utils.Constants;
+
+import java.util.HashMap;
+
+/**
+ * Created by mhigh on 8/14/2017.
+ */
+
+public class ShoppingList {
+    String listName;
+    String owner;
+    private HashMap<String, Object> timestampLastChanged;
+
+    public ShoppingList() {
+    }
+
+    public ShoppingList(String listName, String owner) {
+        this.listName = listName;
+        this.owner = owner;
+        HashMap<String, Object> timestampLastChangedObj = new HashMap<String, Object>();
+        timestampLastChangedObj.put(Constants.FIREBASE_PROPERTY_TIMESTAMP, ServerValue.TIMESTAMP);
+        this.timestampLastChanged = timestampLastChangedObj;
+    }
+
+    public String getListName() {
+        return listName;
+    }
+
+    public String getOwner() {
+        return owner;
+    }
+
+    public HashMap<String, Object> getTimestampLastChanged() {
+        return timestampLastChanged;
+    }
+
+    @JsonIgnore
+    public long getTimestampLastChangedLong(){
+        return (long) timestampLastChanged.get(Constants.FIREBASE_PROPERTY_TIMESTAMP);
+    }
+}
